4d8c02d66428e97a1963982c14187ea5327309a3
Natanael Lima <taelima1997@gmail.com>
2021-03-02 23:51:11 -0300

    create cadastro user service

---
+++ src/services/UserCadastroService.js
@@ -0,0 +1,22 @@
+import db from '../db';
+
+class UserCadastroService {
+  constructor(db) {
+    this.db = db;
+  }
+
+  async createUser({ data }) {
+    const idUser = await db('users').insert(data);
+
+    const user = await db('users').where('id', idUser);
+
+    return user[0];
+  }
+
+  async deleteUser({ id }) {
+    const result = await db('users').where('id', id).del();
+    return !!result;
+  }
+}
+
+export default new UserCadastroService(db);
