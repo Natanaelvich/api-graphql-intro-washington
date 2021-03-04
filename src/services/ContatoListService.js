82d07477409e305892a19a59ccbb1288b6a413bd
Natanael Lima <taelima1997@gmail.com>
2021-03-04 00:15:06 -0300

    create findByUser

--- src/services/ContatoListService.js
+++ src/services/ContatoListService.js
@@ -11,6 +11,9 @@ class ContatoListService {
   async findOne({ id }) {
     return await db('contatos').where('id', id)[0];
   }
+  async findByUser(user) {
+    return await db('contatos').where('user_id', user.id);
+  }
 }
 
 export default new ContatoListService(db);
