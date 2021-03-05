76a01f54d783ca3acbc2f54ef1db221248918ea7
Natanael Lima <taelima1997@gmail.com>
2021-03-04 23:58:54 -0300

    change resolvers with dataSource

--- src/graphql/modules/contatos/resolvers.ts
+++ src/graphql/modules/contatos/resolvers.ts
@@ -1,31 +1,32 @@
 export default {
   Query: {
-    contatos: async (_, __, context) => {
-      const contatos = await context.contatoListService.findAll();
+    contatos: async (_, __, { dataSources, user_id }) => {
+      const contatos = await dataSources.contatoListService.findAll(user_id);
       return contatos;
     },
-    contato: async (_, { id }, context) => {
-      const contato = await context.contatoListService.findOne({ id });
+    contato: async (_, { id }, { dataSources }) => {
+      const contato = await dataSources.contatoListService.findOne({ id });
       return contato;
     },
   },
   Mutation: {
-    createContato: async (_, { data }, context) => {
-      const contato = await context.contatoCadastroService.createContato({
+    createContato: async (_, { data }, { dataSources, user_id }) => {
+      const contato = await dataSources.contatoCadastroService.createContato({
         data,
+        user_id,
       });
       return contato;
     },
-    updateContato: async (_, { id, data }, context) => {
-      const contato = await context.contatoCadastroService.updateContato({
+    updateContato: async (_, { id, data }, { dataSources }) => {
+      const contato = await dataSources.contatoCadastroService.updateContato({
         id,
         data,
       });
       return contato;
     },
-    deleteContato: async (_, { id }, context) => {
-      const contatoDeleted = await context.contatoCadastroService.deleteContato(
-        { id }
+    deleteContato: async (_, { id }, { dataSources, user_id }) => {
+      const contatoDeleted = await dataSources.contatoCadastroService.deleteContato(
+        { id, user_id }
       );
       return contatoDeleted;
     },
